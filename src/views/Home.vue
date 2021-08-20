2743e31f12177582ee5ba8d1a695ddef4b972656
PowerFactor <ton0869310583@gmail.com>
2021-08-21 01:18:01 +0700

    add

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="home">
+    <b-row class="m-0"
+      ><b-col cols="12" class="m-2">
+        <b-button block pill variant="success">success</b-button></b-col
+      ><b-col cols="12" class="m-2">
+        <b-button block pill variant="info">info</b-button></b-col
+      ><b-col cols="12" class="m-2">
+        <b-button block pill variant="danger">danger</b-button></b-col
+      ></b-row
+    >
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+  components: {},
+  beforeCreate() {
+    this.$liff.init({ liffId: "home" });
+    if (!this.$liff.isLoggedIn()) {
+      this.$liff.login({ redirectUri: "https://example.com/path1/path2/" });
+    }
+  },
+  mounted() {
+    this.startline();
+  },
+  methods: {
+    async startline() {
+      // if (!liff.isLoggedIn()) {
+      //   liff.login({ redirectUri: "https://example.com/path1/path2/" });
+      // }
+    },
+  },
+};
+</script>
